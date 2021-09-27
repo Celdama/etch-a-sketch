38794edf131175a35f3dea9da05936689647e6c3
Celdama <celdama.dev@gmail.com>
2021-09-27 10:48:23 +0200

    change display flex to display grid, its more easier to display random square

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const app = document.querySelector('#app');
 
-const makeGrid = (numOfSquare) => {
-  for (let i = 0; i < numOfSquare; i++) {
+const changeColor = () => {
+  console.log('h"e');
+}
+
+const makeGrid = (size) => {
+  app.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  app.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  for (let i = 0; i < size * size; i++) {
     const div = document.createElement('div');
+    div.addEventListener('mouseover', changeColor)
     div.classList.add('square');
     app.appendChild(div);
   }
 };
 
-makeGrid(256)
+makeGrid(64)
 
 
 
 
-const squares = Array.from(document.querySelectorAll('.square'));
-squares.forEach(square => {
-  square.addEventListener('mouseover', (e) => {
-    e.target.style.backgroundColor = 'cadetblue'
-  })
-})
+// const squares = Array.from(document.querySelectorAll('.square'));
+// squares.forEach(square => {
+//   square.addEventListener('mouseover', (e) => {
+//     e.target.style.backgroundColor = 'cadetblue'
+//   })
+// })
 
